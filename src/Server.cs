529d270f38e3aca011d1026e31600dbf53bd3262
Hoang Nguyen <hoangng71299@gmail.com>
2024-07-16 00:04:48 +0700

    Async version

--- src/Server.cs
+++ src/Server.cs
@@ -7,4 +7,4 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+await server.AcceptSocketAsync(); // wait for client
