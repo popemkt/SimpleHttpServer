35bfc0797fc127ec763e6e87a5aae84d0361f991
Hoang Nguyen <hoangng71299@gmail.com>
2024-07-16 23:09:23 +0700

    test

--- src/Server.cs
+++ src/Server.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -10,7 +11,68 @@ TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 var socket = await server.AcceptSocketAsync(); // wait for client
 var buffer = new byte[1024];
-var request = socket.ReceiveAsync(buffer);
-var response = "HTTP/1.1 200 OK\r\n\r\n";
-await socket.SendAsync(Encoding.Default.GetBytes(response));
+var _ = socket.ReceiveAsync(buffer);
+var decodedString = Encoding.Default.GetString(buffer);
+var request = ExtractHandleRequestString(decodedString);
+var response = ExtractHandleResponseString(request);
+await socket.SendAsync(Encoding.Default.GetBytes(response.ToString()));
 socket.Close();
+
+Request ExtractHandleRequestString(string decodedString)
+{
+    var parsedRequest = new Request();
+    string[] lines = decodedString.Split("\r\n");
+    foreach (string line in lines)
+    {
+        if (line.StartsWith("GET /"))
+        {
+            parsedRequest.Path = line.Substring(4);
+            parsedRequest.Method = Method.GET;
+        }
+    }
+
+    return parsedRequest;
+}
+
+Response ExtractHandleResponseString(Request request)
+{
+    return request.Path switch
+    {
+        "/" => new Response { StatusCode = 200 },
+        _ => new Response { StatusCode = 404, Body = "" }
+    };
+}
+
+public class Request
+{
+    public string Path { get; set; }
+    public Method Method { get; set; }
+}
+
+public enum Method
+{
+    GET,
+}
+
+public class Response
+{
+    public string Body { get; set; }
+    public int StatusCode { get; set; }
+    public string ContentType { get; set; }
+    public Method Method { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Method} {StatusCode} {StatusCodes.Description[StatusCode]} {ContentType}\r\n\r\n{Body}";
+    }
+}
+
+public static class StatusCodes
+{
+    public static Dictionary<int, string> Description = new()
+    {
+        { 200, "OK" },
+        { 404, "Not Found" },
+        { 500, "Internal Server Error" }
+    };
+}
